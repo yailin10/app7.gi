4efe9a4777afc9c2f0d1f3bf36bb0a3f52db0982
clarissaasv0102 <38216408+clarissaasv0102@users.noreply.github.com>
2018-05-24 18:02:03 -0500

    subida4
    Aplicaciopn 7

--- js/acciones.js
+++ js/acciones.js
@@ -4,7 +4,7 @@ $(document).ready(function(e) {
 		$('#izquierda').on("swipeleft",function(){ //deslizar a la izquiera
 			navigator.notification.alert("deslizó hacia la izquierda",function(){"aplicacion 7", "OK"}); //aparece mensaje alert
 		}); //cierraswipe inquierdo
-		$('#Derecha').on("swiperight",function(){ //deslizar la derecha
+		$('#derecha').on("swiperight",function(){ //deslizar la derecha
 			navigator.notification.confirm("¿Que quieres hacer?",function(op){ //aparece mensaje para confirmar
 				switch(op) //variable op para las acciones
 				{
